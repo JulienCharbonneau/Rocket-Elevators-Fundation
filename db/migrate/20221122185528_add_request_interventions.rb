41d455fd9d48e2fe4809fda94d62d21684688100
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-11-25 10:30:36 -0500

    change start and end date column to stting

--- db/migrate/20221122185528_add_request_interventions.rb
+++ db/migrate/20221122185528_add_request_interventions.rb
@@ -8,8 +8,8 @@ class AddRequestInterventions < ActiveRecord::Migration[5.2]
       t.integer :column_id
       t.integer :elevator_id
       t.integer :employee_id, null: false
-      t.date :start_date
-      t.date :end_date
+      t.string :start_date
+      t.string :end_date
       t.string :result, :default =>  "Incomplete "
       t.text :report, null: false
       t.string :status, :default =>  "Pending"
