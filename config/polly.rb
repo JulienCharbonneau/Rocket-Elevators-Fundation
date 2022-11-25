7fc1584417f48636256da15d6fb95da340ff747d
JulienCharbonneau <juliencharbonneauqc@gmail.com>
2022-11-25 17:46:58 -0500

    change api key for polly

--- config/polly.rb
+++ config/polly.rb
@@ -1,12 +1,12 @@
-# require 'aws-sdk-core'
+require 'aws-sdk-core'
 
-# Aws.config.update(
-#   region: 'us-east-2',
-#   credentials: Aws::Credentials.new('AKIARG7FYWXXY3AUME5N', 'weYoEuVqSVNrWaSiFRmYHeC8fWG5fmlzKu1a8czW')
-# )
+Aws.config.update(
+  region: 'us-east-1',
+  credentials: Aws::Credentials.new('AKIAQ7JF4W2FZGIY2GVZ', 'vfQntFKRF7LflCpdCYG43J51gad+WMHwZo4aBq8R')
+)
 
-# client = Aws::Polly::Client.new(region: 'us-east-1')
-# client.synthesize_speech("all to read")
+client = Aws::Polly::Client.new(region: 'us-east-1')
+client.synthesize_speech("all to read")
 
-# client.synthesize_speech(output_format: 'mp3', text: 'hello there', voice_id: 'Joanna')
+client.synthesize_speech(output_format: 'mp3', text: 'hello there', voice_id: 'Joanna')
 
